6f12bdf2715112a19206d3de08838d8880a34203
Akshay9623 <akshaysupekar2020@gmail.com>
2024-10-17 09:54:50 +0530

    first time add file

---
+++ src/com/abc/A.java
@@ -0,0 +1,22 @@
+package com.abc;
+
+public class A {
+
+	{
+		System.out.println("non-static block");
+	}
+	static
+	{
+		System.out.println("static block");
+	}
+public static void main(String args[])
+{
+
+	System.out.println("main method start");
+	A a = new A();
+	System.out.println("main method end");
+	A a1 = new A();
+	B b = new B();
+	B b1 = new B();
+}
+}
---
+++ src/com/sit/C.java
@@ -0,0 +1,9 @@
+package com.sit;
+
+public class C {
+	int c=30;
+public void m3()   // user define method
+{
+	System.out.println("m3-----C");
+}
+}
